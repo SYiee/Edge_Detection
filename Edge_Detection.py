abe8c1f99a97e2c6b8f09aa24d870afef52abba7
SYiee <zyx2009@naver.com>
2023-12-25 05:57:15 +0900

    Update Edge_Detection.py

--- Edge_Detection.py
+++ Edge_Detection.py
@@ -87,29 +87,6 @@ def show(img, result1, result2, result, thr_result):
     plt.show()
 
 
-import numpy as np
-
-
-def generate_gaussian_filter(sigma: Union[int, float], filter_shape: Union[list, tuple, None]):
-    # 'sigma' is the standard deviation of the gaussian distribution
-
-    # Extracting filter_shape dimensions with default values
-    m, n = filter_shape if filter_shape is not None else (3, 3)
-    m_half = m // 2
-    n_half = n // 2
-
-    # initializing the filter
-    gaussian_filter = np.zeros((m, n), np.float32)
-
-    # generating the filter
-    for y in range(-m_half, m_half + 1):
-        for x in range(-n_half, n_half + 1):
-            normal = 1 / (2.0 * np.pi * sigma**2.0)
-            exp_term = np.exp(-(x**2.0 + y**2.0) / (2.0 * sigma**2.0))
-            gaussian_filter[y+m_half, x+n_half] = normal * exp_term
-    return gaussian_filter
-
-
 
 # edge detection function
 def edge_detection(img, filter_x, filter_y, threshold, show_img=True):
@@ -159,49 +136,73 @@ def edge_detection(img, filter_x, filter_y, threshold, show_img=True):
 #edge_detection(img, LoG_9, np.zeros_like(LoG_9), threshold=2000)
 #edge_detection(img, gaussian, np.zeros_like(gaussian), threshold=100)
 
+###################################################################################
 
-
-# sigma_value = 1.0
-# filter_shape_value = (5, 5)
-
-# # 가우시안 필터 생성
-# gaussian_filter_result = generate_gaussian_filter(sigma_value, filter_shape_value)
-# print("Output using print():")
-# print(gaussian_filter_result)
-# print()
-
-def create_LoG_filter(sigma, filter_shape):
-    # 필터의 중심 좌표 계산
-    center_x = (filter_shape[1] - 1) / 2
-    center_y = (filter_shape[0] - 1) / 2
-
-    # LoG 필터 생성
-    y, x = np.indices(filter_shape)
-    x = x - center_x
-    y = y - center_y
-
-    # Gaussian 함수를 이용하여 가중치 계산
-    gaussian = np.exp(-(x**2 + y**2) / (2 * sigma**2))
+def gaussian_filter(filter_size, sigma):
+    # get numpy array with identical values in both directions (axis)
+    y, x = np.ogrid[-filter_size:filter_size+1, -filter_size:filter_size+1]
     
-    # Laplacian 필터 계산
-    laplacian = (x**2 + y**2 - 2 * sigma**2) / (sigma**4)
+    # get 3 x 3 matrix based on x and y, then use equation shown to compute filter
+    filter_ = np.exp(-(x**2 + y**2) / (2 * sigma**2)) / (2 * np.pi * sigma**2)
     
-    # LoG 필터 = Gaussian * Laplacian
-    log_filter = gaussian * laplacian
-
-    # 필터 정규화
-    log_filter = log_filter - np.mean(log_filter)
-    log_filter = log_filter / np.sum(np.abs(log_filter))
-
-    return log_filter
-
-sigma_value = 1.0
-filter_shape_value = (3, 3)
-log_filter_example = create_LoG_filter(sigma_value, filter_shape_value)
-
-print("LoG Filter:")
-print(log_filter_example)
+    # comment out if you want to see what is going on.
+    print(f'x looks: {x}\n y looks: \n{y}\n')
+    print('x**2 looks: {0}\n y**2 looks: \n{1}\n'.format(x**2, y**2))
+    print('By the property of numpy array, x**2 + y**2 is: \n{}'.format(x**2 + y**2))
+        
+    return filter_
 
 
+# double derivative kernels 
+def laplacian_filter(kind='laplacian'):
+    if kind == 'laplacian':
+        filter_ = np.array([[0, 1, 0], 
+                        [1, -4, 1], 
+                        [0, 1, 0]])
+    elif kind == 'diag_laplacian':
+        filter_ = np.array([[1, 1, 1], 
+                [1, -8, 1], 
+                [1, 1, 1]])
+        
+    return filter_
 
-edge_detection(img, LoG_5, np.zeros_like(LoG_5), threshold=400)
+def laplacian_edge_detection(image, kind='laplacian', thresh=20):
+    # get filter
+    filter_ = laplacian_filter(kind)
+    
+    # get result array
+    filtered = np.zeros(image.shape)
+    
+    # preprocess image
+    image = image_preprocess(image)
+    
+    # loop through every pixel 
+    for i in range(1, image.shape[0]-1):
+        for j in range(1, image.shape[1]-1):
+            temp_arr = np.array([])
+            left, right = j - 1, j + 2
+            upper, lower = i - 1, i + 2
+            
+            # get matrix for small segment of image
+            for r in range(upper, lower):
+                temp_arr = np.append(temp_arr, image[r][left:right])
+            
+            temp_arr = temp_arr.reshape(3, 3)
+            
+            # calculate gradient
+            grad = np.sum(filter_ * temp_arr)
+            
+            if grad > thresh:
+                filtered[i-1][j-1] = 255
+            else:
+                filtered[i-1][j-1] = 0
+                
+    return filtered
+
+#laplacian_img = laplacian_edge_detection(image_mug, 'laplacian', 20)
+#plot_image(laplacian_img, 'Laplacian edged image')
+
+a = gaussian_filter(1,1)
+print(a)
+
+#edge_detection(img, LoG_5, np.zeros_like(LoG_5), threshold=400)
